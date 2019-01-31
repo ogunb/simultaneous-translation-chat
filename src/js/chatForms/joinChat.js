6a32a828730f7c9064d05a685bf24fe9a7b9ea92
ogunb <babacanogun@gmail.com>
2019-01-31 23:01:12 +0300

    change textContent instead of adding new dom element.

--- src/js/chatForms/joinChat.js
+++ src/js/chatForms/joinChat.js
@@ -10,24 +10,16 @@ function JoinChat() {
   async function onSubmit(e) {
     user.username = e.target[0].value;
     user.lang = e.target[1].value;
-    const inputGroupNode = document.querySelector('.chat__input-group');
-    const chatForm = document.querySelector('.chat__form');
     const userExists = await checkUsers(hashId, user.username);
     if (user.username === ('' || ' ')) {
-      const invalidErrNode = document
-        .createRange()
-        .createContextualFragment(
-          `<span class="form-is-invalid">Dude, you can't have a “space” as a username.</span>`
-        );
-      chatForm.insertBefore(invalidErrNode, inputGroupNode);
+      document.querySelector(
+        '.form-is-invalid'
+      ).textContent = `Dude, you can't have a “space” as a username.`;
       e.target[0].classList.add('is-invalid');
     } else if (userExists) {
-      const invalidErrNode = document
-        .createRange()
-        .createContextualFragment(
-          `<span class="form-is-invalid">The nick already exists in this room dude.</span>`
-        );
-      chatForm.insertBefore(invalidErrNode, inputGroupNode);
+      document.querySelector(
+        '.form-is-invalid'
+      ).textContent = `This nick is already taken in this room dude, sorry.`;
       e.target[0].classList.add('is-invalid');
     } else {
       // sessionStorage.setItem('chat-nick', e.target[0].value);
