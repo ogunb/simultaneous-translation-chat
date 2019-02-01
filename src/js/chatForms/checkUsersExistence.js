b04a4a6bdce54e774874d4333fd039ed4379f442
ogunb <babacanogun@gmail.com>
2019-02-01 03:49:45 +0300

    add createFunction function that creates the room on database with hashId.

--- src/js/chatForms/checkUsersExistence.js
+++
@@ -1,9 +0,0 @@
-import firebase from '../base';
-
-export async function checkUsersExistence(hash, userNick) {
-  let exist = true;
-  await firebase.ref(`${hash}/users/${userNick}`).once('value', snapshot => {
-    if (snapshot.val() === null) exist = false;
-  });
-  return exist;
-}
