6fc4f1362b09f2322be82e95a3b8fb26c74f47e5
ogunb <babacanogun@gmail.com>
2019-02-03 18:59:56 +0300

    Add  function to save languages in database for ease-of-use.

--- src/js/app.js
+++ src/js/app.js
@@ -10,23 +10,24 @@ export const TRANSLATE_API_KEY = process.env.TRANSLATE_API_KEY;
 /* eslint-enable */
 
 LoadLanguages();
-const userNick = sessionStorage.getItem('chat-nick');
+const user = JSON.parse(sessionStorage.getItem('chat-user'));
+const hash = window.location.hash.substring(1);
 const chatForm = document.querySelector('.chat__form');
 const loading = document.querySelector('.loading');
 const landing = document.querySelector('.chat__main-page');
-if (!window.location.hash) {
+if (!hash) {
   loading.style.display = 'none';
   landing.style.display = 'flex';
   chatForm.addEventListener('submit', e => {
     e.preventDefault();
     CreateChat().onSubmit(e);
   });
-} else if (window.location.hash && !userNick) {
+} else if (hash && !user) {
   JoinChat().init();
   chatForm.addEventListener('submit', e => {
     e.preventDefault();
     JoinChat().onSubmit(e);
   });
 } else {
-  directToChat();
+  directToChat(hash, user);
 }
