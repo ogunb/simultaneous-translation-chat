dd568cfc0a7d932168f19aedcff3dce6bd37e426
ogunb <babacanogun@gmail.com>
2019-02-04 23:21:38 +0300

    Deprecate cloud-functions. You can`t make an external API call on a free plan.

--- functions/index.js
+++
@@ -1,7 +0,0 @@
-const functions = require('firebase-functions');
-
-exports.translateMessage = functions.database
-	.ref('/originalMessages/{originalMessage}')
-	.onCreate((snapshot, context) => {
-		const originalMessageObj = snapshot.val();
-	});
